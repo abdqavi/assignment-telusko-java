52d3df906743c8fb9176b2462bb9bf59196131c2
abdqavi <55969459+abdqavi@users.noreply.github.com>
2019-12-14 01:07:31 +0530

    Add files via upload

---
+++ iteration1.java
@@ -0,0 +1,20 @@
+
+public class iteration1 
+{
+	public static void main(String[] args)
+	{	char c=65;
+		for(int i=1;i<4;i++)
+		{	
+			c=65;
+			for(int j=1;j<i+1;j++)
+			{
+				System.out.print(c+" ");
+				c++;
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
